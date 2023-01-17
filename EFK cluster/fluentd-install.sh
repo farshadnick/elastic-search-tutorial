ddee5d4439290e32c1b65f35ac4cf0bb5b81d253
Farshad Nick <88557305+farshadnick@users.noreply.github.com>
2023-01-17 22:46:10 +0330

    Create fluentd-install.sh

---
+++ EFK cluster/fluentd-install.sh
@@ -0,0 +1,7 @@
+
+
+wget  http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
+dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
+
+curl -fsSL https://toolbelt.treasuredata.com/sh/install-ubuntu-focal-td-agent4.sh | sh
+
